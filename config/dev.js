ef87e90da2fa5f048143d549053410913bd89d96
tempguy <mytempmail00767@gmail.com>
2021-03-08 13:43:03 +0530

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,9 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+//  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+//  mongoURI: 'mongodb+srv://task-app:1234asdf@cluster0.ynye7.mongodb.net/test?authSource=admin&replicaSet=atlas-533jjm-shard-0&////readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+  mongoURI: 'mongodb://127.0.0.1:27017/adv_node',  
   cookieKey: '123123123',
+  redisUrl : 'redis://127.0.0.1:6379'
 };
